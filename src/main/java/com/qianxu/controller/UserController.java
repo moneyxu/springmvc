a5ffa210e749e688e11bfeab725ea52602a2936b
qianxu <fedora123@126.com>
2017-07-14 00:00:35 +0800

    url

--- src/main/java/com/qianxu/controller/UserController.java
+++ src/main/java/com/qianxu/controller/UserController.java
@@ -4,6 +4,8 @@ import com.qianxu.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,11 +17,23 @@ public class UserController {
         return "user/register";
     }
 
-    @RequestMapping(method = RequestMethod.POST,value = "/createUser")
-    public ModelAndView createUser(User user){
+    @RequestMapping(method = RequestMethod.POST, value = "/createUser")
+    public ModelAndView createUser(User user) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("user/success");
         modelAndView.addObject(user);
         return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test", params = {"userId"})
+    @ResponseBody
+    public String test1(@RequestParam("userId") String userId) {
+        return "this is userId=" + userId;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test", params = {"customerId"})
+    @ResponseBody
+    public String test2(@RequestParam("customerId") String customerId) {
+        return "this is customerId=" + customerId;
     }
 }
