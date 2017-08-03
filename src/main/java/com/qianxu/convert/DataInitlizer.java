3a128d34ce90af16aedf8396109bea119a59f4a8
qianxu <xu.qian@iqunxing.com>
2017-08-03 14:41:41 +0800

    WebBindingInitializer

--- src/main/java/com/qianxu/convert/DataInitlizer.java
+++ src/main/java/com/qianxu/convert/DataInitlizer.java
@@ -6,11 +6,9 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.WebBindingInitializer;
 import org.springframework.web.context.request.WebRequest;
 
-/**
- * Created by qianxu on 2017/8/3.
- */
-public class DataInitlizer implements WebBindingInitializer{
+public class DataInitlizer implements WebBindingInitializer {
+
     public void initBinder(WebDataBinder binder, WebRequest request) {
-        binder.registerCustomEditor(Rabbit.class,new RabbitEditor());
+        binder.registerCustomEditor(Rabbit.class, new RabbitEditor());
     }
 }
