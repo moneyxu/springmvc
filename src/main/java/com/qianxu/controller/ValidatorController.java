3d8d420f9c3ae3372feb01c066bd04fc2520f1f5
qianxu <xu.qian@iqunxing.com>
2017-08-23 09:53:07 +0800

    validate

--- src/main/java/com/qianxu/controller/ValidatorController.java
+++ src/main/java/com/qianxu/controller/ValidatorController.java
@@ -4,6 +4,7 @@ import com.qianxu.entity.JSR303;
 import com.qianxu.entity.UserRegister;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,7 +27,10 @@ public class ValidatorController {
     }
 
     @RequestMapping(value = "/api/validate/userRegister")
-    public String userRegister(@Valid @ModelAttribute("register") UserRegister userRegister, BindingResult bindingResult){
+    public String userRegister( @ModelAttribute("register") UserRegister userRegister, BindingResult bindingResult){
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult,"userName","required");
+        if("a".equals(userRegister.getUserName()))
+            bindingResult.reject("userName","用户已注册");
         if(bindingResult.hasErrors()){
             return "user/registerError";
         }else{
@@ -38,4 +42,5 @@ public class ValidatorController {
     public UserRegister getUserRegister(){
         return new UserRegister();
     }
+
 }
