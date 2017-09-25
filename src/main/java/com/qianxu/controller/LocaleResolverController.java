04541e3787eb0280bc60927b2dfe673ac6a76639
qianxu <fedora123@126.com>
2017-09-26 00:15:01 +0800

    CookieLocaleResolver

--- src/main/java/com/qianxu/controller/LocaleResolverController.java
+++ src/main/java/com/qianxu/controller/LocaleResolverController.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.support.RequestContext;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Locale;
 
 @Controller
@@ -20,5 +21,12 @@ public class LocaleResolverController {
         RequestContext requestContext = new RequestContext(request);
         Locale locale = RequestContextUtils.getLocaleResolver(request).resolveLocale(request);
         return requestContext.getMessage("locale");
+    }
+
+    @RequestMapping(value = "/api/localeResolver/test2",method = RequestMethod.GET)
+    @ResponseBody
+    public String test2(HttpServletRequest request){
+        RequestContext context = new RequestContext(request);
+        return context.getMessage("locale");
     }
 }
